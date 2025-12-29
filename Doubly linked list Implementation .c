8bc080fb04b95c9d5a9c715051d853ba8767f137
neseka22 <nesekaponnusamy22@gmail.com>
2025-12-29 15:30:33 +0530

    Create Doubly linked list Implementation .c

---
+++ Doubly linked list Implementation .c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+struct node *head = NULL;
+
+void append(int data) {
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        struct node *temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+}
+
+void display() {
+    struct node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+void backward() {
+    struct node *temp = head;
+
+    if (temp == NULL)
+        return;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+}
+
+int main() {
+    int n, data;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &data);
+        append(data);
+    }
+
+    printf("Forward Display: ");
+    display();
+
+    printf("\nBackward Display: ");
+    backward();
+
+    return 0;
+}
